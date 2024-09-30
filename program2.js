d37a1b1cf7f8d0ab4c036cf235df753111e01cb4
unnatideep <161854201+unnatideep@users.noreply.github.com>
2024-09-30 14:17:45 +0530

    Update program2.js

--- program2.js
+++ program2.js
@@ -3,8 +3,35 @@
  * @return {number}
  */
 var romanToInt = function(s) {
+    // Mapping of Roman numerals to their integer values
+    const romanMap = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
     
+    let total = 0;
+    let prevValue = 0;
+    
+    // Iterate over the Roman numeral string in reverse
+    for (let i = s.length - 1; i >= 0; i--) {
+        const currentValue = romanMap[s[i]];
+        
+        // Determine if we should add or subtract the current value
+        if (currentValue < prevValue) {
+            total -= currentValue; // Subtract if the current value is less than the previous
+        } else {
+            total += currentValue; // Otherwise, add it
+        }
+        
+        prevValue = currentValue; // Update the previous value
+    }
+    
+    return total;
 };
 
-
-module.exports={romanToInt}
+module.exports = { romanToInt };
